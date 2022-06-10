ff41a3925f141f2c0d4b34292bd0351df30fc13f
BrandotheCluff <brandon.cluff2@gmail.com>
2022-06-09 22:14:22 -0600

    node drills

--- node-drills-1/app-2/practice.js
+++ node-drills-1/app-2/practice.js
@@ -1,5 +1,12 @@
-//require module
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+    res.end('My birthday is tomorrow!');
+});
+
+server.listen(3001, () => {
+console.log('Server is running');
+});
 // Save the result of the createServer() method in a variable named server.
 
 // Call the server's listen() method with the port number 3001.
