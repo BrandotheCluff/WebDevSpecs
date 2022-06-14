d847097e836e95b8a880115ab0ca7e11308d41a6
BrandotheCluff <brandon.cluff2@gmail.com>
2022-06-14 16:55:37 -0600

    figuring axios

--- buff-cluff-capstone/coach-buff-cluff/src/App.js
+++ buff-cluff-capstone/coach-buff-cluff/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import useAxios from './hooks/useAxios';
+import { useEffect } from 'react';
+import axios from 'axios'
 import Coaching from './components/Coaching'
 import WorkoutBuilder from './components/WorkoutBuilder';
 
 function App() {
+  const info = useAxios('bodyPartList')
+  // const back = useAxios('bodyPart/back')
+  // const cardio = useAxios('bodyPart/cardio')
+  // const chest = useAxios('bodyPart/chest')
+  // const lowerArms = useAxios('bodyPart/lower-arms')
+  // const upperArms = useAxios('bodyPart/upper-arms')
+  // const lowerLegs = useAxios('bodyPart/lower-legs')
+  // const upperLegs = useAxios('bodyPart/upper-legs')
+  // const shoulders = useAxios('bodyPart/shoulders')
+
+useEffect(() => { 
+})
+
   return (
     <div className="App">
     </div>
---
+++ buff-cluff-capstone/coach-buff-cluff/src/hooks/useAxios.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const useAxios = (url) => {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+       let options = {
+            method: 'GET',
+            url: `https://exercisedb.p.rapidapi.com/exercises/${url}`,
+            headers: {
+                'X-RapidAPI-Key': 'e8565680e9msh721a60b51d1533cp184228jsn2a4a6a37a7bc',
+                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+              }
+            };
+        
+        axios.request(options).then((response) => {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    })
+    return data
+}
+
+export default useAxios
